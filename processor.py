b88a06bfe7f23cad1cdeb6aa9f0f80009974c760
Ahmdfdhilah <ahmad.121140173@student.itera.ac.id>
2025-02-17 15:11:08 +0700

    fix easyocr

--- processor.py
+++ processor.py
@@ -1,11 +1,4 @@
-import cv2
-import numpy as np
-from easyocr import Reader
-import asyncio
 import logging
-from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor
-import os
 from config.settings import Settings
 
 logger = logging.getLogger(__name__)
